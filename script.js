1720b5b924fdc749f7cd1398ec5c6cf4572d8495
aguscassano <90217332+aguscassano@users.noreply.github.com>
2025-09-15 18:01:54 -0300

    parallax2

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const elemento1 = document.getElementById("parallax");
+let target = 0;
+let current = 0;
+const velocidad = 0.1; // entre 0 y 1, cuanto más chico más suave
 
 window.addEventListener("scroll", () => {
-    let posicion = window.scrollY || document.documentElement.scrollTop;
-    requestAnimationFrame(() => {
-    elemento1.style.transform = `translateY(${posicion * 0.1}px)`;
-    });
+  target = window.scrollY * 0.1;
 });
+
+function animar() {
+    current += (target - current) * velocidad; // interpolación
+    elemento1.style.transform = `translateY(${current}px)`;
+    requestAnimationFrame(animar);
+}
+
+animar();
